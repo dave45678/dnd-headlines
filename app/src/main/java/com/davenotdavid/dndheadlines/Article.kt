0f382a8fc3007932a9a593ac16926ae16bef9d14
Dave Park <davepark37@gmail.com>
2017-07-01 16:32:50 -0400

    Convert Article class to Kotlin

---
+++ app/src/main/java/com/davenotdavid/dndheadlines/Article.kt
@@ -0,0 +1,8 @@
+package com.davenotdavid.dndheadlines
+
+/**
+ * Custom data class that represents an article (title, URL, URL of its thumbnail-image, and its
+ * publish date) from a news source.
+ */
+data class Article (val title: String, val url: String, val urlToImage: String,
+                    val publishedAt: String)
